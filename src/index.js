c15b91bd5bfa89ef3b497aa0ba61a6814d2ad2fe
Hassan Mef <156674177+Hassan-Mef@users.noreply.github.com>
2024-09-09 14:43:22 +0500

    added slash commands

--- src/index.js
+++ src/index.js
@@ -28,13 +28,70 @@ client.on('messageCreate' , (message) =>{
 
     }
     if (message.content === 'Shit') {
-        message.reply('Hey! NO Swearing 😡' );
+        message.reply('Hey! NO Swearing 😡' )
 
     }
     if(message.content === 'gg') {
         message.react('😀')
     }
+    if(message.content === 'Good Morning') {
+        message.reply(' Good Morning @everyone')
+    }
+   if (message.content === "nigga") {
+     // Replace USER_ID with the actual user's ID
+     const userId = "1120256018874650735"; // Example user ID
+
+     // Mention the user in the reply
+     message.reply(`oh the only nigga i know is  <@${userId}>`);
+   }
+
+
     
-})
+});
+
+// client.on('interactionCreate' ,  (interaction) => {
+//     if (!interaction.isChatInputCommand()) return ;
+
+//     if (interaction.commandName === 'hey') {
+//         interaction.reply('Hey big man');
+//     }
+// });
+
+client.on("interactionCreate", async (interaction) => {
+  if (!interaction.isCommand()) return;
+
+  if (interaction.commandName === "cr") {
+    
+    const roleId = "1277942802763878453";
+
+    
+    const roleMention = `<@&${roleId}>`;
+
+    
+    await interaction.channel.send(
+      `Attention, ${roleMention}! Someone is looking for you !!.`
+    );
+  }
+});
+
+
+client.on("interactionCreate", async (interaction) => {
+  if (!interaction.isCommand()) return;
+
+  const { commandName, options } = interaction;
+
+  if (commandName === "ping") {
+    
+    const userId = options.getUser("user").id;
+
+
+    const user = await client.users.fetch(userId);
+
+    
+    interaction.reply(`Hello, ${user}! 🌟`);
+  }
+});
+
+
 client.login(process.env.TOKEN);
 
---
+++ src/register-commands.js
@@ -0,0 +1,45 @@
+require('dotenv').config();
+const { REST, Routes } = require("discord.js");
+
+const commands = [
+  {
+    name: "hey",
+    description: "Replies with hey!",
+  },
+  {
+    name: "cr",
+    description: "Mentions the CR",
+  },
+  {
+    name: "ping",
+    description: "Pings a user",
+    options: [
+      {
+        name: "user",
+        description: "User to ping",
+        type: 6, // Type 6 represents a user
+        required: true, // Set to true if you want this option to be mandatory
+      },
+    ],
+  },
+];
+
+
+const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
+
+(async () => {
+  try {
+    console.log('Resgistering slah Commands...')
+    await rest.put(
+      Routes.applicationGuildCommands(
+        process.env.CLIENT_ID,
+        process.env.GUILD_ID
+      ),
+      { body: commands }
+    );
+
+    console.log('Slash Commands registered')
+  } catch (error) {
+    console.log(`There was an error ${error}`);
+  }
+})();
